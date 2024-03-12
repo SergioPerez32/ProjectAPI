4a9fdff2f10be25c14a13b70a7288544a477707d
SergioPerez32 <sergiorlando10@hotmail.com>
2024-03-12 02:34:28 -0400

    Added endpoint to create a secret

--- task1.js
+++ task1.js
@@ -1,4 +1,5 @@
 //Dependencies
+const bodyParser = require('body-parser');
 const express = require('express');
 
 
@@ -9,6 +10,23 @@ app.use(bodyParser.json());
 
 //Database
 const storage = {}
+
+
+//Functions
+function generateSecretKey() {
+    return Math.floor(Math.random() * 1000000);
+}
+
+
+//Endpoints
+app.post('/secrets', (req, res) => {
+    const { message } = req.body;
+    const secretKey = generateSecretKey();
+    const secret = { message };
+    storage[secretKey] = secret;
+    const urlForSecret = `http://localhost:3000/secrets/`+secretKey ;
+    res.status(201).json({ secretKey, urlForSecret});
+  });
 
 
 //Launching express app
